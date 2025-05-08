15f7714810bfe179afe5f3f14292bc83e02b85df
Sebastian <miguel.poveda1220@gmail.com>
2025-05-08 00:02:25 -0500

    feat: :bug: Solucion de mala implementacion de la interface en el servicio

--- infrastructure/repositories/DClienteRepository.cs
+++ infrastructure/repositories/DClienteRepository.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using sgi_app.domain.ports;
 using sgi_app.infrastructure.mysql;
+using sgi_app.infrastructure.repositories;
 using sgi_app_v1.domain.dto;
 using sgi_app_v1.domain.ports;
 
@@ -13,41 +15,71 @@ namespace sgi_app_v1.infrastructure.repositories
     {
         private readonly ConexionSingleton _conexion;
 
-        public DClienteRepository(ConexionSingleton conexion)
+    public DClienteRepository(string connectionString)
         {
-            _conexion = conexion;
+            _conexion = ConexionSingleton.Instancia(connectionString);
         }
+
 
         public List<ClienteDto> GetAll()
         {
-            throw new NotImplementedException();
+            var clientes = new List<ClienteDto>();
+            var connec = _conexion.ObtenerConexion();
+            var query = "SELECT t.id AS tercero_id,t.nombre,t.apellidos,t.email,t.tipo_doc_id,t.tipo_tercero_id,t.ciudad_id,c.id AS cliente_id,c.fecha_nac,c.fecha_ultima_compra FROM terceros t INNER JOIN cliente c ON c.tercero_id = t.id;";
+            using (var command = new MySqlCommand(query, connec))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var cliente = new ClienteDto()
+                        {
+                            Id = reader.GetString("tercero_id"),
+                            Nombre = reader.GetString("nombre"),
+                            Apellidos = reader.GetString("apellidos"),
+                            Email = reader.GetString("email"),
+                            TipoDoc = reader.GetInt32("tipo_doc_id"),
+                            TipoTercero = reader.GetInt32("tipo_tercero_id"),
+                            CiudadId = reader.GetInt32("ciudad_id"),
+                            FechaNac = reader.GetDateTime("fecha_nac"),
+                            FechaUCompra = reader.GetDateTime("fecha_ultima_compra")
+                        };
+                        clientes.Add(cliente);
+                    }
+        
+        }    
+            }
+             return clientes;
         }
-        public Task<ClienteDto> Add(ClienteDto entity)
+       
+        
+        public void Add(ClienteDto entity)
         {
-            throw new NotImplementedException();
+            var connec = _conexion.ObtenerConexion();
+            string query = "CALL insertar_tercero_y_cliente(@id, @nombre, @apellidos, @email, @tipo_doc_id, @tipo_tercero_id, @ciudad_id, @fecha_nac, @fecha_ultima_compra)";
+            using (var command = new MySqlCommand(query, connec))
+            {
+                command.Parameters.AddWithValue("@id", entity.Id);
+                command.Parameters.AddWithValue("@nombre", entity.Nombre);
+                command.Parameters.AddWithValue("@apellidos", entity.Apellidos);
+                command.Parameters.AddWithValue("@email", entity.Email);
+                command.Parameters.AddWithValue("@tipo_doc_id", entity.TipoDoc);
+                command.Parameters.AddWithValue("@tipo_tercero_id", entity.TipoTercero);
+                command.Parameters.AddWithValue("@ciudad_id", entity.CiudadId);
+                command.Parameters.AddWithValue("@fecha_nac", entity.FechaNac);
+                command.Parameters.AddWithValue("@fecha_ultima_compra", entity.FechaUCompra);
+                command.ExecuteNonQuery();
+            }
+          
         }
-        public Task<ClienteDto> Update(ClienteDto entity)
+        public void Update(ClienteDto entity)
         {
-            throw new NotImplementedException();
+        
         }
-        public Task<ClienteDto> Delete(ClienteDto entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IGenericRepository<ClienteDto>.Add(ClienteDto entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IGenericRepository<ClienteDto>.Update(ClienteDto entity)
-        {
-            throw new NotImplementedException();
-        }
-
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+       
         }
+
     }
 }

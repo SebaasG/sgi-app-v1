86e249be9f015860dff0a4f13f030c5674576a99
SebaasG <miguel.poveda1220@gmail.com>
2025-05-09 08:57:02 -0500

    feat: :construction: Creacion de carpetas para venta

---
+++ application/services/VentaService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sgi_app_v1.application.services
+{
+    public class VentaService
+    {
+        
+    }
+}
---
+++ application/ui/VentaConsoleUI.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sgi_app_v1.application.ui
+{
+    public class VentaConsoleUI
+    {
+        
+    }
+}
